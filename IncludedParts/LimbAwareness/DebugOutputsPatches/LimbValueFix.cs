d2404251c9a0b13e5a468bb62f9d259ac188e1f4
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-08-21 21:53:17 +0200

    products value include target limb configuration

---
+++ IncludedParts/LimbAwareness/DebugOutputsPatches/LimbValueFix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using System.Reflection;
+
+namespace MSE2.HarmonyPatches
+{
+    [HarmonyPatch]
+    internal static class LimbValueFix
+    {
+        [HarmonyTargetMethod]
+        internal static MethodInfo TargetMethod ()
+        {
+            return AccessTools.Method( typeof( DebugOutputsEconomy ), "CheapestProductsValue" );
+        }
+
+        [HarmonyPostfix]
+        internal static void AddSubpartValue ( ref float __result, RecipeDef d )
+        {
+            var target = d.GetModExtension<TargetLimb>()?.targetLimb;
+
+            if ( target != null )
+            {
+                foreach ( ThingDefCountClass thingDefCountClass in d.products )
+                {
+                    var compProp = thingDefCountClass.thingDef.GetCompProperties<CompProperties_IncludedChildParts>();
+
+                    if ( thingDefCountClass.count == 1 && compProp != null )
+                    {
+                        foreach ( var includedPart in compProp.AllPartsForLimb( target ) )
+                        {
+                            __result += includedPart.GetStatValueAbstract( StatDefOf.MarketValue );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
