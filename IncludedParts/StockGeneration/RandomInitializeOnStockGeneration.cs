9666fb21bc4cff8eca4a4920bbe1f974ae6f43a7
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-08-23 16:50:58 +0200

    stock generation

---
+++ IncludedParts/StockGeneration/RandomInitializeOnStockGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace MSE2.HarmonyPatches
+{
+    [HarmonyPatch( typeof( StockGeneratorUtility ) )]
+    [HarmonyPatch( nameof( StockGeneratorUtility.TryMakeForStockSingle ) )]
+    internal static class RandomInitializeOnStockGeneration
+    {
+        [HarmonyPostfix]
+        internal static void RandInit ( Thing __result )
+        {
+            var comp = __result.TryGetComp<CompIncludedChildParts>();
+
+            if ( comp != null )
+            {
+                comp.InitializeForLimb( comp.Props.installationDestinations.RandomElement() );
+            }
+        }
+    }
+}
