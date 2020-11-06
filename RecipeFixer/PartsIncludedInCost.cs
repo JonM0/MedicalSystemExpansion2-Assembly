0744286dfeb02aff7a552ea90a0279c0409b9733
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-11-06 23:06:03 +0100

    worktomake fixing

--- RecipeFixer/PartsIncludedInCost.cs
+++ RecipeFixer/PartsIncludedInCost.cs
@@ -11,19 +11,32 @@ namespace MSE2.RecipeFixer
 {
     internal class PartsIncludedInCost : DefModExtension
     {
-        internal List<ThingDefCountClass> partsInCost;
+        internal List<ThingDefCountClass> partsInCost = new List<ThingDefCountClass>();
 
-        internal float minToLeave = 0;
+        internal float minToLeave = 0.2f;
 
+        internal float baseWorkFactor = 1;
+
+        [Unsaved]
         internal bool wasFixed = false;
 
         internal FixMode fixMode = FixMode.CostList;
 
         public override IEnumerable<string> ConfigErrors ()
         {
+            if ( baseWorkFactor < 0 )
+            {
+                yield return "[MSE2] baseWorkFactor has to be positive. Value: " + baseWorkFactor;
+            }
+
+            if ( baseWorkFactor != 1f && fixMode != FixMode.CostList )
+            {
+                yield return "[MSE2] baseWorkFactor does nothing when fixMode is not CostList. Value: " + baseWorkFactor;
+            }
+
             if ( minToLeave < 0 || minToLeave > 1 )
             {
-                yield return "[MSE2] minToLeave has to be between 0. and 1.";
+                yield return "[MSE2] minToLeave has to be between 0. and 1. Value: " + minToLeave;
             }
 
             if ( !wasFixed )
@@ -32,27 +45,27 @@ namespace MSE2.RecipeFixer
             }
         }
 
-        internal IEnumerable<ThingDefCountClass> RecursivePartsInCost
-        {
-            get
-            {
-                if ( this.partsInCost != null )
-                {
-                    for ( int i = 0; i < partsInCost.Count; i++ )
-                    {
-                        var part = partsInCost[i];
-                        yield return part;
+        //internal IEnumerable<ThingDefCountClass> RecursivePartsInCost
+        //{
+        //    get
+        //    {
+        //        if ( this.partsInCost != null )
+        //        {
+        //            for ( int i = 0; i < partsInCost.Count; i++ )
+        //            {
+        //                var part = partsInCost[i];
+        //                yield return part;
 
-                        var rec = part.thingDef.GetModExtension<PartsIncludedInCost>();
-                        if ( rec != null )
-                            foreach ( var subpart in rec.RecursivePartsInCost )
-                            {
-                                yield return subpart;
-                            }
-                    }
-                }
-            }
-        }
+        //                var rec = part.thingDef.GetModExtension<PartsIncludedInCost>();
+        //                if ( rec != null )
+        //                    foreach ( var subpart in rec.RecursivePartsInCost )
+        //                    {
+        //                        yield return subpart;
+        //                    }
+        //            }
+        //        }
+        //    }
+        //}
 
         internal enum FixMode
         {
--- RecipeFixer/RecipeFixingUtilities.cs
+++ RecipeFixer/RecipeFixingUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,42 +10,62 @@ namespace MSE2.RecipeFixer
 {
     public static class RecipeFixingUtilities
     {
+        private static void FixCostList ( ThingDef thingDef, PartsIncludedInCost partsDefMod, List<ThingDefCountClass> allPartsList )
+        {
+            foreach ( (ThingDef part, int totalOfPart) in from part in allPartsList // all the parts that need their cost removed from here
+                                                          from cost in part.thingDef.costList // the ingredients of those parts
+                                                          group (cost.thingDef, cost.count * part.count / (part.thingDef.recipeMaker?.productCount ?? 1)) by cost.thingDef into tc // group them by the thingdef
+                                                          select (tc.Key, tc.Select( x => x.Item2 ).Sum()) ) // add up all the total by thingdef
+            {
+                // take the corresponding price item
+                var originalPartCost = thingDef.costList.Find( x => x.thingDef == part );
+                if ( originalPartCost != null )
+                {
+                    // decrement it by totalOfPart, but leave at least the minimum specified in partsDefMod rounded up
+                    originalPartCost.count = Math.Max( (int)Math.Ceiling( originalPartCost.count * partsDefMod.minToLeave ), originalPartCost.count - totalOfPart );
+
+                    if ( originalPartCost.count == 0 ) thingDef.costList.Remove( originalPartCost );
+                }
+            }
+        }
+
+        private static void FixWorkToMake ( ThingDef thingDef, PartsIncludedInCost partsDefMod, List<ThingDefCountClass> allPartsList )
+        {
+            var work = thingDef.GetStatValueAbstract( StatDefOf.WorkToMake );
+            work *= partsDefMod.baseWorkFactor;
+            //var temp = allPartsList.Select( p => p.thingDef.GetStatValueAbstract( StatDefOf.WorkToMake ) ).ToList();
+            var workOfParts = allPartsList.Select( p => p.thingDef.GetStatValueAbstract( StatDefOf.WorkToMake ) * p.count ).Sum();
+            work = Math.Max( work * partsDefMod.minToLeave, work - workOfParts );
+            thingDef.SetStatBaseValue( StatDefOf.WorkToMake, work );
+        }
+
         internal static void FixRecipe ( ThingDef thingDef, PartsIncludedInCost partsDefMod )
         {
-            var allPartsList = partsDefMod.RecursivePartsInCost.ToList();
+            var allPartsList = partsDefMod.partsInCost.ToList();
 
             foreach ( var part in allPartsList )
             {
                 EnsureFixedRecipe( part.thingDef );
             }
 
+            Log.Message( "[MSE2] Fixing costList of " + thingDef.defName );
+
             switch ( partsDefMod.fixMode )
             {
-                case PartsIncludedInCost.FixMode.CostList:
-                    foreach ( (ThingDef part, int totalOfPart) in from part in allPartsList // all the parts that need their cost removed from here
-                                                                  from cost in part.thingDef.costList // the ingredients of those parts
-                                                                  group (cost.thingDef, cost.count * part.count / (part.thingDef.recipeMaker?.productCount ?? 1)) by cost.thingDef into tc // group them by the thingdef
-                                                                  select (tc.Key, tc.Select( x => x.Item2 ).Sum()) ) // add up all the total by thingdef
-                    {
-                        // take the corresponding price item
-                        var originalPartCost = thingDef.costList.Find( x => x.thingDef == part );
-                        if ( originalPartCost != null )
-                        {
-                            // decrement it by totalOfPart, but leave at least the minimum specified in partsDefMod rounded up
-                            originalPartCost.count = Math.Max( (int)Math.Ceiling( originalPartCost.count * partsDefMod.minToLeave ), originalPartCost.count - totalOfPart );
-                        }
-                    }
-                    break;
+            case PartsIncludedInCost.FixMode.CostList:
+                FixWorkToMake( thingDef, partsDefMod, allPartsList );
+                FixCostList( thingDef, partsDefMod, allPartsList );
+                break;
 
-                case PartsIncludedInCost.FixMode.MarketValue:
-                    var valueOfParts = allPartsList.Select( p => p.thingDef.BaseMarketValue * p.count ).Sum();
-                    thingDef.BaseMarketValue = Math.Max( thingDef.BaseMarketValue * partsDefMod.minToLeave, thingDef.BaseMarketValue - valueOfParts );
+            case PartsIncludedInCost.FixMode.MarketValue:
+                var valueOfParts = allPartsList.Select( p => p.thingDef.BaseMarketValue * p.count ).Sum();
+                thingDef.BaseMarketValue = Math.Max( thingDef.BaseMarketValue * partsDefMod.minToLeave, thingDef.BaseMarketValue - valueOfParts );
 
-                    break;
+                break;
 
-                default:
-                    Log.Error( "" );
-                    break;
+            default:
+                Log.Error( "Unsupported FixMode" );
+                break;
             }
 
             partsDefMod.wasFixed = true;
@@ -55,7 +76,6 @@ namespace MSE2.RecipeFixer
             var partsIncluded = thingDef.GetModExtension<PartsIncludedInCost>();
             if ( partsIncluded != null && !partsIncluded.wasFixed )
             {
-                Log.Message( "[MSE2] Fixing costList of " + thingDef.defName );
                 FixRecipe( thingDef, partsIncluded );
             }
         }
