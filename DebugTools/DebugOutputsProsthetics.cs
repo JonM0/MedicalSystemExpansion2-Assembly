a4b63860fb1b920f0437d169b6f0f840240904c0
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-11-06 22:54:59 +0100

    add debug table to test costlist fixing

--- DebugTools/DebugOutputsProsthetics.cs
+++ DebugTools/DebugOutputsProsthetics.cs
@@ -5,11 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse;
 using RimWorld;
+using System.Runtime.InteropServices;
 
 namespace MSE2.DebugTools
 {
     public static class DebugOutputsProsthetics
     {
+        private static string DefName ( ThingDef thingDef )
+        {
+            return thingDef.defName;
+        }
+
         private static string StandardChildren ( ThingDef thingDef )
         {
             return thingDef.GetCompProperties<CompProperties_IncludedChildParts>()?.standardChildren.Select( c => c.defName ).ToCommaList() ?? "";
@@ -32,8 +38,28 @@ namespace MSE2.DebugTools
             return thingDef.BaseMarketValue;
         }
 
-        [DebugOutput]
-        public static void ProstheticSegmentCosts ()
+        private static float AverageChildSegmentValue ( ThingDef thingDef )
+        {
+            return thingDef.GetCompProperties<CompProperties_IncludedChildParts>()?.standardChildren.Select( t => t.BaseMarketValue ).Average() ?? -1;
+        }
+
+        private static float AvgConfigValue ( ThingDef thingDef )
+        {
+            return thingDef.GetCompProperties<CompProperties_IncludedChildParts>()?.AverageValue ?? -1;
+        }
+
+        private static float WorkToMake ( ThingDef thingDef )
+        {
+            return thingDef.GetStatValueAbstract( StatDefOf.WorkToMake );
+        }
+
+        private static float AvgChildWorkToMake ( ThingDef thingDef )
+        {
+            return thingDef.GetCompProperties<CompProperties_IncludedChildParts>()?.standardChildren.Select( t => t.GetStatValueAbstract( StatDefOf.WorkToMake ) ).Average() ?? -1;
+        }
+
+        [DebugOutput( "MSE2" )]
+        public static void ProstheticCosts ()
         {
             IEnumerable<ThingDef> dataSources = (from d in DefDatabase<ThingDef>.AllDefs
                                                  where d.isTechHediff && !d.costList.NullOrEmpty()
@@ -41,13 +67,68 @@ namespace MSE2.DebugTools
 
             TableDataGetter<ThingDef>[] getters = new TableDataGetter<ThingDef>[]
             {
-                new TableDataGetter<ThingDef>("defName", (ThingDef d) => d.defName),
+                new TableDataGetter<ThingDef>("defName", DefName),
                 new TableDataGetter<ThingDef>("StandardChildren", StandardChildren),
                 new TableDataGetter<ThingDef>("SegmentCostList", SegmentCostList),
                 new TableDataGetter<ThingDef>("SegmentValue", SegmentValue),
             };
 
             DebugTables.MakeTablesDialog( dataSources, getters );
+        }
+
+        private static IEnumerable<ThingDef> AllProstheticsAndChildren ()
+        {
+            HashSet<ThingDef> allThings = new HashSet<ThingDef>( from d in DefDatabase<ThingDef>.AllDefs
+                                                                 where d.isTechHediff && !d.costList.NullOrEmpty()
+                                                                    && d.GetCompProperties<CompProperties_IncludedChildParts>() != null
+                                                                 select d );
+            Queue<ThingDef> Q = new Queue<ThingDef>( allThings );
+
+            while ( Q.Any() )
+            {
+                var comp = Q.Dequeue().GetCompProperties<CompProperties_IncludedChildParts>();
+
+                if ( comp != null )
+                {
+                    foreach ( var child in comp.standardChildren )
+                    {
+                        if ( allThings.Add( child ) )
+                        {
+                            Q.Enqueue( child );
+                        }
+                    }
+
+                    if ( comp.alwaysInclude != null )
+                    {
+                        foreach ( var child in comp.alwaysInclude )
+                        {
+                            if ( allThings.Add( child ) )
+                            {
+                                Q.Enqueue( child );
+                            }
+                        }
+                    }
+                }
+            }
+
+            return allThings;
+        }
+
+        [DebugOutput( "MSE2" )]
+        public static void ProstheticSegmentValues ()
+        {
+            TableDataGetter<ThingDef>[] getters = new TableDataGetter<ThingDef>[]
+            {
+                new TableDataGetter<ThingDef>("defName", DefName),
+                new TableDataGetter<ThingDef>("StandardChildren", StandardChildren),
+                new TableDataGetter<ThingDef>("SegmentValue", SegmentValue),
+                new TableDataGetter<ThingDef>("AverageChildSegmentValue", AverageChildSegmentValue),
+                new TableDataGetter<ThingDef>("AvgConfigValue", AvgConfigValue),
+                new TableDataGetter<ThingDef>("WorkToMake", WorkToMake),
+                new TableDataGetter<ThingDef>("AvgChildWorkToMake", AvgChildWorkToMake),
+            };
+
+            DebugTables.MakeTablesDialog( AllProstheticsAndChildren(), getters );
         }
     }
 }
