22e8be76219da8f13e4f7f538a9cc2a32c7da552
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-12-16 01:08:22 +0100

    add documentation, remove unused commented code

--- GenList.cs
+++ GenList.cs
@@ -8,6 +8,14 @@ namespace MSE2
 {
     public static class GenList
     {
+        /// <summary>
+        /// Removes the first element that matches the conditions defined by the specified predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the list</typeparam>
+        /// <param name="list">The list from where to remove the element</param>
+        /// <param name="predicate">The <c>System.Predicate</c> delegate that defines the conditions of the elements to remove.</param>
+        /// <returns><see langword="true"/> if item is successfully removed; otherwise, <see langword="false"/>. This method also returns <see langword="false"/> if item was not found in the <c>System.Collections.Generic.List</c>.</returns>
+        /// <exception cref="System.ArgumentNullException">if predicate is null</exception>
         public static bool Remove<T> ( this List<T> list, Predicate<T> predicate )
         {
             int index = list.FindIndex( predicate );
@@ -22,40 +30,5 @@ namespace MSE2
                 return false;
             }
         }
-
-        //public static int FindSequenceIndex<T> ( this List<T> list, List<T> sequence ) where T : IEquatable<T>
-        //{
-        //    if ( list is null )
-        //    {
-        //        throw new ArgumentNullException( nameof( list ) );
-        //    }
-
-        //    if ( sequence is null )
-        //    {
-        //        throw new ArgumentNullException( nameof( sequence ) );
-        //    }
-
-        //    int matched = 0;
-
-        //    for ( int i = 0; i < list.Count; i++ )
-        //    {
-
-        //        if ( list[i].Equals( sequence[matched] ) )
-        //        {
-        //            if ( ++matched == sequence.Count )
-        //            {
-        //                return i - matched + 1;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            i -= matched;
-        //            matched = 0;
-        //        }
-        //        i++;
-        //    }
-
-        //    return -1;
-        //}
     }
 }
