66494fe6c958f831e6f0753e1b13edec2390c338
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-12-17 20:00:37 +0100

    capacity tooltip ordering

---
+++ SortAndGroupPawnCapacityTip.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace MSE2.HarmonyPatches
+{
+    [HarmonyPatch( typeof( HealthCardUtility ) )]
+    [HarmonyPatch( nameof( HealthCardUtility.GetPawnCapacityTip ) )]
+    internal static class SortAndGroupPawnCapacityTip
+    {
+        private static readonly StringBuilder internalStringBuilder = new StringBuilder();
+
+        [HarmonyPostfix]
+        private static void SortAndGroup ( ref string __result )
+        {
+            var lines = __result.Split( '\n' );
+
+            if ( lines.Length > 5 ) // has more than 1 impactor (+3 starting lines and +1 empty line at the end)
+            {
+                // copy starting 3 lines
+                StringBuilder newResult = internalStringBuilder.Clear()
+                    .AppendLine( lines[0] )
+                    .AppendLine( lines[1] )
+                    .AppendLine( lines[2] );
+
+                Array.Sort( lines, 3, lines.Length - 4 ); // sort impactors
+
+                string prev = lines[3];
+                int count = 1;
+
+                // loop from second impactor to last impactor
+                for ( int i = 4; i < lines.Length - 1; i++ )
+                {
+                    var line = lines[i];
+
+                    if ( prev != line )
+                    {
+                        newResult.Append( prev );
+                        if ( count > 1 )
+                        {
+                            newResult.Append( " (x" ).Append( count ).Append( ')' );
+                        }
+                        newResult.AppendLine();
+
+                        count = 1;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                    prev = line;
+                }
+
+                newResult.Append( prev );
+                if ( count > 1 )
+                {
+                    newResult.Append( " (x" ).Append( count ).Append( ')' );
+                }
+                newResult.AppendLine();
+
+                __result = newResult.ToString();
+            }
+        }
+    }
+}
