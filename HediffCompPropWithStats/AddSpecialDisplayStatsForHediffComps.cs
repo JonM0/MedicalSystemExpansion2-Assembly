cadf0806a39b4f8532f7918bde2658f395338a4a
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-12-19 00:53:22 +0100

    HediffComp DisplayStats

---
+++ HediffCompPropWithStats/AddSpecialDisplayStatsForHediffComps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+
+using HarmonyLib;
+
+namespace MSE2
+{
+    [HarmonyPatch( typeof( HediffDef ) )]
+    [HarmonyPatch( nameof( HediffDef.SpecialDisplayStats ) )]
+    internal static class AddSpecialDisplayStatsForHediffComps
+    {
+        [HarmonyPostfix]
+        internal static void AddCompStats ( HediffDef __instance, ref IEnumerable<StatDrawEntry> __result, StatRequest req )
+        {
+            var comps = __instance.comps;
+            if ( comps != null )
+            {
+                for ( int i = 0; i < comps.Count; i++ )
+                {
+                    if ( comps[i] is IHediffCompPropsWithStats compWithStats )
+                    {
+                        __result = __result.Concat( compWithStats.SpecialDisplayStats( req ) );
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HediffCompPropWithStats/IHediffCompWithStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+
+namespace MSE2
+{
+    internal interface IHediffCompPropsWithStats
+    {
+        IEnumerable<StatDrawEntry> SpecialDisplayStats ( StatRequest req );
+    }
+}
