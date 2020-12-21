c9cdd8eeb177358755a4483e9f4235c6e0dac706
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-12-21 21:18:18 +0100

    auto recipe users

---
+++ AutoRecipeUser/AutoRecipeUserUtilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+
+namespace MSE2
+{
+    internal static class AutoRecipeUserUtilities
+    {
+        internal static void ApplyAutoRecipeUsers ()
+        {
+            try
+            {
+                List<ThingDef> pawndefs = (from t in DefDatabase<ThingDef>.AllDefs
+                                           where t.race != null
+                                           select t).ToList();
+
+                foreach ( (RecipeDef recipedef, AutoRecipeUsers aru) in from d in DefDatabase<RecipeDef>.AllDefs
+                                                                        where d.HasModExtension<AutoRecipeUsers>()
+                                                                        select (d, d.GetModExtension<AutoRecipeUsers>()) )
+                {
+                    for ( int i = 0; i < pawndefs.Count; i++ )
+                    {
+                        ThingDef pawndef = pawndefs[i];
+                        if ( !recipedef.recipeUsers.Contains( pawndef ) && // is not already present
+                            aru.IsValidRace( pawndef ) && // AutoRecipeUsers allows the race
+                            ((recipedef.appliedOnFixedBodyParts.NullOrEmpty() && recipedef.appliedOnFixedBodyPartGroups.NullOrEmpty()) // can be installed on any part
+                            || recipedef.appliedOnFixedBodyParts.Exists( part => pawndef.race.body.AllParts.Exists( p => p.def == part ) ) // has a part of a compatible def
+                            || recipedef.appliedOnFixedBodyPartGroups.Exists( group => pawndef.race.body.AllParts.Exists( p => p.IsInGroup( group ) ) )) )
+                        {
+                            recipedef.recipeUsers.Add( pawndef );
+                        }
+                    }
+
+                    Log.Message( recipedef.defName + " AutoRecipeUsers" +
+                        "\n new: " + string.Join( ", ", recipedef.recipeUsers.Select( u => (u, u.race.body.AllParts.Count) ) ) );
+                }
+            }
+            catch ( Exception e )
+            {
+                Log.Error( "[MSE2] Error applying AutoRecipeUsers: " + e.Message );
+            }
+        }
+    }
+}
---
+++ AutoRecipeUser/AutoRecipeUsers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+
+using Verse;
+
+namespace MSE2
+{
+    internal class AutoRecipeUsers : DefModExtension
+    {
+        private FloatRange bodySizeRange = FloatRange.One;
+        private List<ThingDef> neverInclude;
+        private Intelligence minIntelligence = Intelligence.Animal;
+        private Intelligence maxIntelligence = Intelligence.Humanlike;
+
+        internal bool IsValidRace ( ThingDef pawnDef )
+        {
+            RaceProperties race = pawnDef.race;
+
+            return race != null
+                && this.bodySizeRange.IncludesEpsilon( race.baseBodySize )
+                && this.minIntelligence <= race.intelligence
+                && this.maxIntelligence >= race.intelligence
+                && (this.neverInclude == null || !this.neverInclude.Contains( pawnDef ));
+        }
+    }
+}
