6fcfb4a4b8cc5356058523bcbb3476b6bd549b75
JonM0 <61517667+JonM0@users.noreply.github.com>
2021-02-06 19:52:51 +0100

    empire mod compatibility

---
+++ ModPatches/Empire/EmpireTitheGeneration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace MSE2.HarmonyPatches
+{
+    [HarmonyPatch]
+    internal static class EmpireTitheGeneration
+    {
+        [HarmonyTargetMethods]
+        internal static IEnumerable<MethodBase> TargetMethods ()
+        {
+            yield return AppDomain.CurrentDomain.GetAssemblies().Reverse()
+                .Select( d => d.GetType( "FactionColonies.settlementWindowFC" ) )
+                .First( t => t != null ).GetMethod( "DrawProductionHeaderLower" );
+
+            yield return AppDomain.CurrentDomain.GetAssemblies().Reverse()
+                .Select( d => d.GetType( "FactionColonies.ResourceFC" ) )
+                .First( t => t != null ).GetMethod( "returnLowestCost" );
+        }
+
+        [HarmonyPrepare]
+        internal static bool Prepare( MethodBase original )
+        {
+            return ModLister.AllInstalledMods.Any( m => m.PackageId.EqualsIgnoreCase( "Saakra.Empire" ) && m.Active );
+        }
+
+        // replace cost for prosthetics with the average complete value
+
+        [HarmonyTranspiler]
+        internal static IEnumerable<CodeInstruction> Transpiler ( IEnumerable<CodeInstruction> instructions )
+        {
+            foreach ( CodeInstruction instruction in instructions )
+            {
+                if ( instruction.Calls( AccessTools.PropertyGetter( typeof( ThingDef ), nameof( ThingDef.BaseMarketValue ) ) ) )
+                {
+                    yield return new CodeInstruction( OpCodes.Call, AccessTools.Method( typeof( EmpireTitheGeneration ), nameof( AvgMarketValue ) ) );
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+        internal static float AvgMarketValue ( ThingDef thingDef )
+        {
+            return thingDef.GetCompProperties<CompProperties_IncludedChildParts>()?.AverageValue ?? thingDef.BaseMarketValue;
+        }
+    }
+}
