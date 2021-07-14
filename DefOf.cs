8c14ecdaf23349496ccd149b530213f11cd36b53
JonM0 <61517667+JonM0@users.noreply.github.com>
2021-07-14 22:39:05 +0200

    rename DefOf

---
+++ DefOf.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+
+using Verse;
+
+namespace MSE2
+{
+    [RimWorld.DefOf]
+    internal static class DefOf
+    {
+        static DefOf ()
+        {
+            DefOfHelper.EnsureInitializedInCtor( typeof( DefOf ) );
+        }
+
+        public static HediffDef MSE_ModuleSlot;
+    }
+}
